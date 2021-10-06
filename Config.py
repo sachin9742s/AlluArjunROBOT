fb95bfd875290860f9ccc8d087b3e2c273cf4bce
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-06 15:57:46 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -34,7 +34,7 @@ DATABASE_NAME = environ['BOT_NAME']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
-default_start_msg = """
+default_start_msg = """<b>Hey {}!!</b>
 **Hi, I'm Auto Filter V3**
 
 Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
